f248f49c47f5ef7a52d31c61c5595212b497f750
Kykyyta <izvekov-denis@bk.ru>
2025-10-19 17:51:31 +0300

    last mb

--- src/math/model/base/AbstractVector.java
+++ src/math/model/base/AbstractVector.java
@@ -14,6 +14,7 @@ public abstract class AbstractVector<T extends AbstractVector<T>> implements Vec
 
     @Override
     public T add(T other) { // сложение
+
         float[] result = new float[components.length];
         for (int i = 0; i < components.length; i++) {
             result[i] = this.components[i] + other.components[i];
@@ -23,59 +24,72 @@ public abstract class AbstractVector<T extends AbstractVector<T>> implements Vec
 
     @Override
     public T subtract(T other) { // вычитание
+
         float[] result = new float[components.length];
         for (int i = 0; i < components.length; i++) {
             result[i] = this.components[i] - other.components[i];
         }
         return createNew(result);
+
     }
 
     @Override
     public T multiply(float scalar) { // скалярка
+
         float[] result = new float[components.length];
         for (int i = 0; i < components.length; i++) {
             result[i] = this.components[i] * scalar;
         }
         return createNew(result);
+
     }
 
     @Override
     public T divide(float scalar) { // деление на скаляр
+
         if (Math.abs(scalar) < 1e-12f) {
             throw new ArithmeticException("Division by zero");
         }
         return multiply(1.0f / scalar);
+
     }
 
     @Override
     public float length() { // длинна вектора
+
         float sum = 0;
         for (float component : components) {
             sum += component * component;
         }
         return (float) Math.sqrt(sum);
+
     }
 
     @Override
     public T normalize() { // нормализация
+
         float len = length();
         if (len < 1e-12f) {
             throw new ArithmeticException("Cannot normalize zero vector");
         }
         return multiply(1.0f / len);
+
     }
 
     @Override
     public float dot(T other) { // скалярное произведение
+
         float result = 0;
         for (int i = 0; i < components.length; i++) {
             result += this.components[i] * other.components[i];
         }
         return result;
+
     }
 
     @Override
     public boolean equals(Object obj) { // сравнение
+
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
 
@@ -88,14 +102,17 @@ public abstract class AbstractVector<T extends AbstractVector<T>> implements Vec
             }
         }
         return true;
+
     }
 
     @Override
     public int hashCode() {
+
         int result = 1;
         for (float component : components) {
             result = 31 * result + Float.hashCode(component);
         }
         return result;
+
     }
 }
