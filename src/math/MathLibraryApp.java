d45491d212d53388acf7b932d1b758cc3b8405a0
Kykyyta <izvekov-denis@bk.ru>
2025-10-19 17:53:33 +0300

    last mb

--- src/math/MathLibraryApp.java
+++ src/math/MathLibraryApp.java
@@ -4,7 +4,9 @@ import math.controller.MathController;
 import math.view.MathInteractiveView;
 
 public class MathLibraryApp {
+
     public static void main(String[] args) {
+
         MathController controller = new MathController();
 
         if (args.length > 0 && "test".equals(args[0])) {
