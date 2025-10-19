90968eb83ec66108f8968a11fbb13b70427d55e4
Kykyyta <izvekov-denis@bk.ru>
2025-10-19 17:53:14 +0300

    last mb

--- src/math/view/LinearAlgebraConsoleView.java
+++ src/math/view/LinearAlgebraConsoleView.java
@@ -12,6 +12,7 @@ public class LinearAlgebraConsoleView extends BaseConsoleView {
 
     public void showLinearAlgebraMenu() {
         while (true) {
+
             showSectionHeader("LINEAR ALGEBRA");
             showMessage("1. Solve Linear System (3x3)");
             showMessage("2. Solve Linear System (4x4)");
@@ -25,11 +26,13 @@ public class LinearAlgebraConsoleView extends BaseConsoleView {
                 case 2 -> solveLinearSystem4x4();
                 case 3 -> { return; }
                 default -> showError("Invalid option!");
+
             }
         }
     }
 
     private void solveLinearSystem3x3() {
+
         showSectionHeader("SOLVE 3x3 LINEAR SYSTEM");
         Matrix3f A = readMatrix3f("Enter coefficient matrix A");
         Vector3f b = readVector3f("Enter right-hand side vector b");
@@ -37,8 +40,6 @@ public class LinearAlgebraConsoleView extends BaseConsoleView {
         try {
             Vector3f x = controller.solveLinearSystem(A, b);
             showMessage("Solution x = " + x);
-
-            // Verification
             Vector3f Ax = controller.multiplyMatrixVector(A, x);
             showMessage("Verification A*x = " + Ax);
             showMessage("Solution is correct: " + Ax.equals(b));
@@ -48,6 +49,7 @@ public class LinearAlgebraConsoleView extends BaseConsoleView {
     }
 
     private void solveLinearSystem4x4() {
+
         showSectionHeader("SOLVE 4x4 LINEAR SYSTEM");
         Matrix4f A = readMatrix4f("Enter coefficient matrix A");
         Vector4f b = readVector4f("Enter right-hand side vector b");
@@ -56,12 +58,13 @@ public class LinearAlgebraConsoleView extends BaseConsoleView {
             Vector4f x = controller.solveLinearSystem(A, b);
             showMessage("Solution x = " + x);
 
-            // Verification
             Vector4f Ax = controller.multiplyMatrixVector(A, x);
             showMessage("Verification A*x = " + Ax);
             showMessage("Solution is correct: " + Ax.equals(b));
+
         } catch (ArithmeticException e) {
             showError(e);
         }
+
     }
 }
