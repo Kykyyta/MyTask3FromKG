3fd3785067648144c64292269e6cbeac140fd158
Kykyyta <izvekov-denis@bk.ru>
2025-10-20 03:19:54 +0300

    nice

--- src/math/MathLibraryApp.java
+++ src/math/MathLibraryApp.java
@@ -6,14 +6,8 @@ import math.view.MathInteractiveView;
 public class MathLibraryApp {
 
     public static void main(String[] args) {
-
         MathController controller = new MathController();
-
-        if (args.length > 0 && "test".equals(args[0])) {
-            controller.runTests();
-        } else {
-            MathInteractiveView view = new MathInteractiveView(controller);
-            view.start();
-        }
+        MathInteractiveView view = new MathInteractiveView(controller);
+        view.start();
     }
 }
